fb2cb658bfdea3196f9256cb56454f284a5e0085
Jay S <jshin49@gmail.com>
2021-11-07 14:02:53 +0000

    fix padding

--- CODS/hf_train_t5.py
+++ CODS/hf_train_t5.py
@@ -454,30 +454,16 @@ class T5Large(nn.Module):
                 answer_tokens = answer_tokens[-self.args.target_max_len+1:] # -1 for <s> or </s>
             
             else:
-                answer_tokens = self.tokenizer(
-                    e.summary,
-                    padding=True,
-                    return_tensors=None, # non-padded return List[List[Int]]
-                    return_attention_mask=False,
-                    truncation=True,
-                    max_length=max_target_len,
-                ).input_ids
-                answer_tokens = [(t if t != config.pad_token_id else -100) for t in answer_tokens]
-                # answer_tokens.masked_fill_(answer_tokens == -100, config.pad_token_id)
-            #     answer_tokens = self.tokenizer.tokenize(e.summary)
-            #     if len(answer_tokens) > max_target_len: max_target_len = len(answer_tokens)
-            #     answer_tokens = answer_tokens[:self.args.target_max_len-1] # -1 for <s> or </s>
+                answer_tokens = self.tokenizer.tokenize(e.summary)
+                if len(answer_tokens) > max_target_len: max_target_len = len(answer_tokens)
+                # answer_tokens = answer_tokens[:self.args.target_max_len-1] # -1 for <s> or </s>
                 
             # answer_tokens_ = self.tokenizer.convert_tokens_to_ids(answer_tokens)
-            # target_ids = answer_tokens_  # <s> ...
-            # target_labels = answer_tokens_ + [config.eos_token_id]  # ... </s>
-            # target_len = len(target_ids)
-            # padding_len = self.args.target_max_len - target_len
-            # target_ids += ([config.pad_token_id] * padding_len)
-            # target_labels += ([-100] * padding_len)  # -100 is the default index to be ignored
-            # assert len(target_ids) == self.args.target_max_len
-            # assert len(target_labels) == self.args.target_max_len
-
+            # target_labels = answer_tokens_ + [config.eos_token_id] # ... </s>
+            padding_len = self.args.target_max_len - len(answer_tokens)
+            answer_tokens += ([-100] * padding_len) # -100 is the default index to be ignored
+            assert len(answer_tokens) == self.args.target_max_len
+            
             # Get functional turns label (truncate to max_len), either only 0/1 or 0-6 modular index
             max_num_of_turns = 50
             # local_max_num_of_turns = source_ids.count(self.tokenizer.bos_token_id)
